f8ccd711b20cbfdcd52e0dfd4e67de871ff2f862
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-11-04 05:37:32 +0530

    Pissu Kolama@Pissu Kollah

---
+++ lib/deploy.sh
@@ -0,0 +1,23 @@
+apt-get update && apt-get upgrade
+apt-get install git -y
+apt-get install nodejs -y
+apt-get install ffmpeg -y
+apt-get install imagemagick -y
+apt-get install yarn
+git clone https://github.com/Mathiya578/Sohan-Matheesha
+cd Sohan-Matheesha 
+npm start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
