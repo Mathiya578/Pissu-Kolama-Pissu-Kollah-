99519b47233980b9545cf0c4d439ad09b76985d0
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-11-06 05:45:43 +0530

    Pissu Kolama@Pissu Kollah

---
+++ commands/General/repo.js
@@ -0,0 +1,32 @@
+
+const { tlang,botpic } = require('../../lib')
+module.exports = {
+    name: 'repo',
+    category: 'general',
+    alias: ["script", "git", "sc"],
+    desc: 'Sends userbot github repo link.',
+    async exec(citel, Void) {
+        
+        let buttonMessaged = {
+            image: { url: await botpic() },
+            caption: `Hey ${citel.pushName}\n*This is Pissu Kolama@Pissu Kollah Repo*\n\n*Repo:* https://github.com/Mathiya578/Pissu-Kolama-Pissu-Kollah-\n\n*Whatsapp Group:* https://chat.whatsapp.com/IX3xBGKd9U44gJmdhsHiHp\n\n*Deploy Your Own:*-\PISSU-KOLAMA-PISSU-KOLLAH.herokuapp.com/deploy `,
+            footer: ` ` + tlang().footer,
+            headerType: 4,
+            contextInfo: {
+              externalAdReply: {
+                title: "Pissu Kolama@Pissu Kollah-Repo",
+                body: "Easy to Use",
+                thumbnail: log0,
+                mediaType: 2,
+                mediaUrl: `https://github.com/Mathiya578/Pissu-Kolama-Pissu-Kollah-`,
+                sourceUrl: `https://github.com/Mathiya578/Pissu-Kolama-Pissu-Kollah-`,
+              },
+            },
+          };
+          await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+          });
+ 
+    }
+ }
+      
